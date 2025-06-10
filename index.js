abf5a110d498095bb313960b2a74b99bd8070f16
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-06-10 18:26:47 +0000

    Added Palindrome logic

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ let submitButton = document.getElementById('check-btn');
 let result = document.getElementById('result');
 
 submitButton.addEventListener('click', () => {
+    event.preventDefault()
+
     if (textInput.value === "" || textInput.value === " "){
         alert('Please Enter A Value')
     }
+   let textInput1 = textInput.value.toLowerCase().replace(/\s+/g, '')
+   let originalWord = textInput1.split('').join('')
+   let reversedWord =  textInput1.split('').reverse().join('')
+   
+   if ( originalWord === reversedWord) {
+      console.log("It Works")
+   } else {
+    console.log("It don't work")
+   }
+       
 }) 
+
